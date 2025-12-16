57ec229b74b056e6f0dc22b54a3f12d5631165c8
tylercschneider <tylercschneider@gmail.com>
2025-12-16 15:03:18 -0500

    add event_type validation to OutboxEvent

--- app/models/event_engine/outbox_event.rb
+++ app/models/event_engine/outbox_event.rb
@@ -3,5 +3,6 @@ module EventEngine
     self.table_name = "event_engine_outbox_events"
 
     validates :event_name, presence: true
+    validates :event_type, presence: true
   end
 end
--- test/models/outbox_event_test.rb
+++ test/models/outbox_event_test.rb
@@ -5,13 +5,19 @@ module EventEngine
     test "persists an outbox event" do
       event = OutboxEvent.create!(
         event_name: "example.event",
+        event_type: "example.event"
       )
 
       assert event.persisted?
     end
 
     test "outbox event is invalid without event_name" do
-      event = OutboxEvent.new()
+      event = OutboxEvent.new(event_type: "example.event")
+
+      assert_not event.valid?
+    end
+    test "outbox event is invalid without event_type" do
+      event = OutboxEvent.new(event_name: "example.event")
 
       assert_not event.valid?
     end
