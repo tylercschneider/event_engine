32aa9dfdeae8de66e85e37e8314cd4c3a0e5588b
tylercschneider <tylercschneider@gmail.com>
2025-12-17 17:27:34 -0500

    EventEmitter argument changes

--- lib/event_engine/event_emitter.rb
+++ lib/event_engine/event_emitter.rb
@@ -1,10 +1,10 @@
 module EventEngine
   class EventEmitter
-    def self.emit(definition:, payload:)
+    def self.emit(definition:)
       OutboxEvent.create!(
         event_name: definition.event_name,
         event_type: definition.event_type,
-        payload: payload
+        payload: definition.payload
       )
     end
   end
--- test/dummy/lib/event_engine/event_emitter_test.rb
+++ test/dummy/lib/event_engine/event_emitter_test.rb
@@ -3,23 +3,21 @@ require "test_helper"
 module EventEngine
   class EventEmitterTest < ActiveSupport::TestCase
     test "creates an OutboxEvent from an EventDefinition and payload" do
-      definition = EventDefinition.new(
+      definition = Class.new(EventDefinition) do
+        def build_payload
+          { random: "filler" }
+        end
+      end.new(
         event_name: "order.shipped",
         event_type: "domain"
       )
 
-      payload = { order_id: 123 }
-
-      outbox_event =
-        EventEmitter.emit(
-          definition: definition,
-          payload: payload
-        )
+      outbox_event = EventEmitter.emit(definition: definition)
 
       assert outbox_event.persisted?
       assert_equal "order.shipped", outbox_event.event_name
       assert_equal "domain", outbox_event.event_type
-      assert_equal payload.stringify_keys, outbox_event.payload
+      assert_equal({ random: "filler" }.stringify_keys, outbox_event.payload)
     end
   end
 end
