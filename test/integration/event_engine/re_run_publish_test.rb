7a374bdbea2803fd0d917fce6660c7c6b98aad1b
tylercschneider <tylercschneider@gmail.com>
2025-12-17 10:11:44 -0500

    Ensure outbox publisher job is safe to re-run

---
+++ test/integration/event_engine/re_run_publish_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class EventEngine::ReRunPublishTest < ActiveSupport::TestCase
+  test "re-running the job does not re-publish events" do
+    event = EventEngine::OutboxEvent.create!(
+      event_type: "OrderCreated",
+      event_name: "order.created",
+      payload: { filler: "x" }
+    )
+
+    transport = EventEngine::Transports::InMemoryTransport.new
+    EventEngine.configure { |c| c.transport = transport }
+
+    # first run
+    EventEngine::PublishOutboxEventsJob.perform_now
+
+    # second run
+    EventEngine::PublishOutboxEventsJob.perform_now
+
+    assert_equal [event], transport.events
+    assert_not_nil event.reload.published_at
+  end
+end
