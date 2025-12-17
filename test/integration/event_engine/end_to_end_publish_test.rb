a84391d2fbaba5826e629bd9a2efebbac4be8dcf
tylercschneider <tylercschneider@gmail.com>
2025-12-17 08:58:19 -0500

    Add end-to-end test for outbox publishing happy path

---
+++ test/integration/event_engine/end_to_end_publish_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class EventEngine::EndToEndPublishTest < ActiveSupport::TestCase
+  test "event is delivered and marked published" do
+    event = EventEngine::OutboxEvent.create!(
+      event_type: "OrderCreated",
+      event_name: "order.created",
+      payload: { filler: "x" }
+    )
+
+    transport = EventEngine::Transports::InMemoryTransport.new
+    publisher = EventEngine::OutboxPublisher.new(transport: transport)
+
+    publisher.call
+
+    assert_equal [event], transport.events
+    assert_not_nil event.reload.published_at
+  end
+end
