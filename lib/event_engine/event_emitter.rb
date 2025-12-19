70fc12ad88fb5d9c8531c5ba56a6f0ae429fbc31
tylercschneider <tylercschneider@gmail.com>
2025-12-19 10:13:49 -0500

    Add EventEmitter to orchestrate registry, builder, and outbox writer

--- lib/event_engine/event_emitter.rb
+++ lib/event_engine/event_emitter.rb
@@ -1,13 +1,9 @@
 module EventEngine
   class EventEmitter
     def self.emit(event_name:, data:)
-      # schema = EventRegistry.current(event_name)
-      # attrs  = EventBuilder.build(
-      #   schema: schema,
-      #   data: data
-      # )
-
-      # OutboxWriter.write(attrs)
+      schema = EventRegistry.current(event_name)
+      attrs  = EventBuilder.build(schema: schema, data: data)
+      OutboxWriter.write(attrs)
     end
   end
 end
--- test/lib/event_engine/event_emitter_test.rb
+++ test/lib/event_engine/event_emitter_test.rb
@@ -1,9 +1,34 @@
 require "test_helper"
+require "ostruct"
 
 module EventEngine
   class EventEmitterTest < ActiveSupport::TestCase
-    test "creates an OutboxEvent from an EventDefinition and payload" do
+    class CowFed < EventDefinition
+      event_name :cow_fed
+      event_type :domain
 
+      input :cow
+
+      required_payload :weight, from: :cow, attr: :weight
+    end
+
+    setup do
+      EventRegistry.reset!
+      EventRegistry.register(CowFed)
+    end
+
+    test "emits an OutboxEvent via registry and builder" do
+      cow = OpenStruct.new(weight: 500)
+
+      event = EventEmitter.emit(
+        event_name: :cow_fed,
+        data: { cow: cow }
+      )
+
+      assert event.persisted?
+      assert_equal "cow_fed", event.event_name
+      assert_equal "domain", event.event_type
+      assert_equal({ "weight" => 500 }, event.payload)
     end
   end
 end
