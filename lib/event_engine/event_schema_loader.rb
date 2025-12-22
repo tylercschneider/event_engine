1f7193cdd2b9efd143ed7ac7db3d4c662e1d8578
tylercschneider <tylercschneider@gmail.com>
2025-12-21 20:33:04 -0500

    harden schemaloader

--- lib/event_engine/event_schema_loader.rb
+++ lib/event_engine/event_schema_loader.rb
@@ -4,15 +4,30 @@ module EventEngine
       registry = FileLoadedRegistry.new
       return registry unless File.exist?(path)
 
-      schema = nil
+      contents = File.read(path.to_s)
+      return registry if contents.strip.empty?
 
-      schema = Module.new.module_eval(File.read(path.to_s), path.to_s)
-      
-      if schema
-        schema.schemas_by_event.each_value do |versions|
-          versions.each_value do |s|
-            registry.register(s)
-          end
+      sandbox = Module.new
+      sandbox.const_set(:EventEngine, EventEngine)
+
+      schema =
+        sandbox.module_eval(contents, path.to_s)
+
+      unless schema.is_a?(EventEngine::EventSchema)
+        raise <<~MSG
+          Invalid EventEngine schema file.
+
+          Expected #{path} to return an EventSchema from:
+            EventEngine::EventSchema.define { ... }
+
+          But got:
+            #{schema.inspect}
+        MSG
+      end
+
+      schema.schemas_by_event.each_value do |versions|
+        versions.each_value do |s|
+          registry.register(s)
         end
       end
 
