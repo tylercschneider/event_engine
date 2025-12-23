d92957ccef357616d76cc99394b60e46398320db
tylercschneider <tylercschneider@gmail.com>
2025-12-23 16:48:57 -0500

    tighten SchemaWriter

--- lib/event_engine/event_schema_writer.rb
+++ lib/event_engine/event_schema_writer.rb
@@ -4,9 +4,10 @@ module EventEngine
       # This file is authoritative in production.
       # It is generated from EventDefinitions via:
       #
-      #   bin/rails event_engine:schema_dump
+      #   bin/rails event_engine:schema:dump
       #
       # Do not edit manually.
+
     RUBY
 
     def self.write(path, event_schema)
@@ -16,18 +17,30 @@ module EventEngine
           .flat_map { |_event, versions| versions.values }
           .sort_by { |s| [s.event_name.to_s, s.event_version] }
 
-      File.open(path, "w") do |f|
-        f.puts HEADER
-        f.puts
-        f.puts "EventEngine::EventSchema.define do |schema|"
+      File.open(path, "w") do |io|
+        io.write(HEADER)
+        io.write("EventEngine::EventSchema.define do |schema|\n")
 
-        schemas.each do |event_schema|
-          f.puts "  schema.register("
-          f.puts "    #{event_schema.to_ruby}"
-          f.puts "  )"
+        schemas.each do |definition|
+          write_definition(io, definition)
         end
 
-        f.puts "end"
+        io.write("end\n")
+      end
+    end
+
+    def self.write_definition(io, definition)
+      io.write("  schema.register(\n")
+      indent(io, 4) { definition.to_ruby }
+      io.write("  )\n")
+    end
+
+    def self.indent(io, spaces)
+      padding = " " * spaces
+      yield.each_line do |line|
+        io.write(padding)
+        io.write(line)
+        io.write("\n")
       end
     end
   end
