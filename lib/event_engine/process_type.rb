ac1bfd39533dcb03797fd3c8dcb4139ebd26d566
tylercschneider <tylercschneider@gmail.com>
2026-06-08 15:26:16 -0400

    Add ProcessType.processor_for mapping

--- lib/event_engine/process_type.rb
+++ lib/event_engine/process_type.rb
@@ -2,8 +2,21 @@ module EventEngine
   module ProcessType
     ALL = %i[inline background durable broker telemetry sourced].freeze
 
+    PROCESSORS = {
+      inline: :subscribers,
+      background: :subscribers,
+      durable: :delivery,
+      broker: :delivery,
+      telemetry: :telemetry,
+      sourced: :sourcing
+    }.freeze
+
     def self.all
       ALL
+    end
+
+    def self.processor_for(type)
+      PROCESSORS[type]
     end
   end
 end
--- test/lib/event_engine/process_type_test.rb
+++ test/lib/event_engine/process_type_test.rb
@@ -5,5 +5,18 @@ module EventEngine
     test "all lists the known process types" do
       assert_equal %i[inline background durable broker telemetry sourced], ProcessType.all
     end
+
+    test "processor_for maps each process type to its owning processor" do
+      expected = {
+        inline: :subscribers,
+        background: :subscribers,
+        durable: :delivery,
+        broker: :delivery,
+        telemetry: :telemetry,
+        sourced: :sourcing
+      }
+
+      assert_equal expected, ProcessType.all.to_h { |type| [type, ProcessType.processor_for(type)] }
+    end
   end
 end
