cee5caec696cca46d1871126cd78d0842da63ce8
tylercschneider <tylercschneider@gmail.com>
2026-07-01 15:27:56 -0400

    Rename register_handler levels: keyword to process_types:

--- test/lib/event_engine/module_dispatch_test.rb
+++ test/lib/event_engine/module_dispatch_test.rb
@@ -7,7 +7,7 @@ class EventEngine::ModuleDispatchTest < ActiveSupport::TestCase
 
   test "dispatches through a handler registered on the module" do
     received = []
-    EventEngine.register_handler(->(event) { received << event }, levels: :all)
+    EventEngine.register_handler(->(event) { received << event }, process_types: :all)
 
     EventEngine.dispatch(EventEngine::Event.new(event_name: :thing_happened, process_type: :inline, payload: {}))
 
