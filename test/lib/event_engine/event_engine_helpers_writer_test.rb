0005a4d877743f0997df1d6873cbf8065612860f
tylercschneider <tylercschneider@gmail.com>
2026-07-01 11:33:25 -0400

    Prove the generated helpers file defines a working emitter

--- test/lib/event_engine/event_engine_helpers_writer_test.rb
+++ test/lib/event_engine/event_engine_helpers_writer_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 require "tempfile"
+require "ostruct"
 
 module EventEngine
   class EventEngineHelpersWriterTest < ActiveSupport::TestCase
+    include EventEngineTestHelpers
     def schema_with(required_inputs:, optional_inputs: [])
       EventSchema.new.tap do |event_schema|
         event_schema.register(
@@ -48,6 +50,30 @@ module EventEngine
       source = generate(schema_with(required_inputs: [:cow]))
 
       assert_includes source, "metadata: metadata"
+    end
+
+    test "the generated file defines a real helper that emits" do
+      helpers_snapshot = snapshot_event_engine_helpers
+      previous_registry = EventEngine.schema_registry
+      event_schema = schema_with(required_inputs: [:cow])
+      EventEngine.schema_registry =
+        SchemaRegistry.new.tap { |registry| registry.load_from_schema!(event_schema) }
+
+      received = []
+      EventEngine.register_handler(->(event) { received << event }, levels: :all)
+
+      Tempfile.create(["helpers", ".rb"]) do |file|
+        EventEngineHelpersWriter.write(file.path, event_schema)
+        load file.path
+
+        EventEngine.cow_fed(cow: OpenStruct.new(weight: 500))
+      end
+
+      assert_equal 1, received.size
+    ensure
+      restore_event_engine_helpers(helpers_snapshot)
+      EventEngine.schema_registry = previous_registry
+      EventEngine.reset_handlers!
     end
   end
 end
